a7e8a17f024d00e7a3bf11e10ae75c40437a9e80
Can Gokmen <cgokmen@scc-va3.scc.bu.edu>
2025-06-26 15:42:29 -0400

    Organized profile_inserts.cpp more

--- profile_inserts.cpp
+++ profile_inserts.cpp
@@ -1,4 +1,3 @@
-
 #include <algorithm>
 #include <chrono>
 #include <fstream>
@@ -14,36 +13,17 @@
 
 using namespace std;
 
-/*
-template <typename key_type>
-std::vector<key_type> read_bin(const char* filename) {
-    std::ifstream inputFile(filename, std::ios::binary);
-    inputFile.seekg(0, std::ios::end);
-    const std::streampos fileSize = inputFile.tellg();
-    inputFile.seekg(0, std::ios::beg);
-    std::vector<key_type> data(fileSize / sizeof(key_type));
-    inputFile.read(reinterpret_cast<char*>(data.data()), fileSize);
-    return data;
-}
-*/
-
 int main(int argc, char** argv) {
-    bool verbose = false;  // optional argument
-    int N = 5000000;       // optional argument
+    int N = 10000000;       // default value
     string input_file;     // required argument
-    // Parse arguments; make sure to increment i by 2 if you consume an argument
+    // Parse only -N argument
     for (int i = 1; i < argc;) {
-        if (string(argv[i]) == "-v") {
-            verbose = true;
-            i++;
-        } else if (string(argv[i]) == "-N") {
+        if (string(argv[i]) == "-N" && i + 1 < argc) {
             N = atoi(argv[i + 1]);
             i += 2;
-        } 
-        /*else if (string(argv[i]) == "-f") {
-            input_file = argv[i + 1];
-            i += 2;
-        }*/
+        } else {
+            i++;
+        }
     }
 
     // read data
@@ -56,15 +36,13 @@ int main(int argc, char** argv) {
 
 
     // Change the type of tree to ART::ART to use ART tree
-    ART::ART* tree = new ART::ART();
+    ART::QuART_tail* tree = new ART::QuART_tail();
 
     for (uint64_t i = 0; i < N; i++) {
         uint8_t key[4];
         ART::loadKey(keys[i], key);
 
         tree->insert(key, keys[i]);
-
-        //cout << i << endl;
                 
         // Uncomment the following lines to verify the tail path after each insertion
         /*
@@ -75,6 +53,5 @@ int main(int argc, char** argv) {
         */    
     }
 
-    //cout << "finished everything" << endl;
     return 0;
 }
